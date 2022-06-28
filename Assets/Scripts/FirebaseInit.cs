be88e1137c17c72f889b11e48db0a8453211d6ab
artvved <artvved2000@mail.ru>
2022-06-28 17:20:29 +0300

    firebase analytics init

---
+++ Assets/Scripts/FirebaseInit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Firebase;
+using UnityEngine;
+using Firebase.Analytics;
+
+public class FirebaseInit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
